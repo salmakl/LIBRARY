3b7de932c795294bc5526f27567ef80cdf558f72
salmakl <salma.kalkhi@gmail.com>
2021-04-09 09:37:55 +0100

    file

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ session_start();
         integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
         crossorigin="anonymous" />
     <script src="https://kit.fontawesome.com/0407d298dc.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>H O M E</title>
 </head>
 
--- library.php
+++ library.php
@@ -8,10 +8,10 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="stylee.css">
+    <link rel="stylesheet" href="css/stylee.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
     <script src="https://kit.fontawesome.com/0407d298dc.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>L I B R A R Y</title>
     
 </head>
@@ -128,7 +128,7 @@ session_start();
       
         </div>
     </footer>
-    <script src="trie.js"></script>
+    <script src="js/trie.js"></script>
     <script src="js/navbar.js"></script>
 </body>
 </html>
